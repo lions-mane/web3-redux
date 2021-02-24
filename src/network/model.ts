1069d0d9e267ae286bfa7a628ab0d8d62b88852c
Leo Vigna <leo.vigna@gmail.com>
2021-02-24 12:00:02 +0100

    Squashed commit of the following:
    commit ed8b5aa540cec66864d3137d5199b02e96dc87c8
    Author: Leo Vigna <leo.vigna@gmail.com>
    Date:   Wed Feb 24 11:22:41 2021 +0100
    
        patch missing license
    
    commit 7f393dd39b0ebe5f70b2334dcc6bca2bcddfc4e4
    Author: Leo Vigna <leo.vigna@gmail.com>
    Date:   Wed Feb 24 11:15:30 2021 +0100
    
        fix npm package (#7)
    
    commit a28c8baffbfaf28a45f58578a8f0ffc7dafa6302
    Author: Leo Vigna <leo.vigna@gmail.com>
    Date:   Wed Feb 24 00:29:59 2021 +0100
    
        Bug fix duplicate (#6)
    
        * fix bugs
    
        * simplify tests
    
        * refactor tests
    
        * Web3 intiialize test
    
        * refactor docs
    
        * update docs
    
        * update argsHash?
    
        * update readme
    
        * patch README
    
        * contract call selector
    
        * default intialize rpc
    
    commit 3899c7d47a30e4ac5d36621b5d9d83e256f7c947
    Author: tetther1122 <tetthera@gmail.com>
    Date:   Fri Feb 19 15:23:51 2021 +1000
    
        add electron
    
    commit 2f5137a8becaac5f5a677d6128ef8e0ca96d83f7
    Author: Leo Vigna <leo.vigna@gmail.com>
    Date:   Mon Feb 15 16:14:58 2021 +0100
    
        Send tx (#4)
    
        * fix packaging
    
        * Save web3Contract in store
    
        * fix typescript error
    
        * refactor event subcription
    
        * send transaction test
    
        * default initialize networks
    
        * refactor imports
    
        * Tests spinup own server
    
    commit ee511bc7c9c35705551e237719976138dad9dac1
    Author: Leo Vigna <leo.vigna@gmail.com>
    Date:   Sun Feb 14 13:31:54 2021 +0100
    
        Web3 configure (#3)
    
        * fix typos
    
        * web3 network add
    
        * auto add doc generation to git
    
        * update readme web3 configure
    
    commit ab752dc1f9bb2fd599dbac28c932f553f66559e2
    Author: Leo Vigna <leo.vigna@gmail.com>
    Date:   Fri Feb 12 15:08:09 2021 +0100
    
        Add docs (#2)
    
        * docs generate
    
        * update readme
    
        * eslintignore docs
    
    commit 0c4091c48ef91fdeceb7f587ee460ed3b21b96f3
    Author: Leo Vigna <leo.vigna@gmail.com>
    Date:   Fri Feb 12 12:32:43 2021 +0100
    
        Web3 block tx (#1)
    
        * basic web3 redux
    
        * add action typeguard
    
        * Index by networkId
    
        * block-subscription
    
        * subsciption cancel multi-network
    
        * update selectors and tests
    
        * update selectors and tests
    
        * contract call
    
        * call method cache key
    
        * block sync contract calls
    
        * fix missing ts-ignore
    
        * contract call transaction sync
    
        * Block subscribe with transactions
    
        * contract event subscription
    
        * event subcription test
    
        * update copyright lionsmane

--- src/network/model.ts
+++ src/network/model.ts
@@ -1,9 +1,22 @@
 import { attr, Model as ORMModel } from 'redux-orm';
 import Web3 from 'web3';
 
+/**
+ * EVM Network Id object.
+ *
+ * @param networkId - A network id.
+ */
 export interface NetworkId {
     networkId: string;
 }
+
+/**
+ * EVM Network object.
+ * @see {@link https://chainid.network//} for a list of popular EVM Networks.
+ *
+ * @param networkId - A network id.
+ * @param web3 - A web3 object.
+ */
 export type Network = {
     networkId: string;
     web3: Web3;
--- src/test/block.sagas.test.ts
+++ src/test/block.sagas.test.ts
@@ -1,44 +1,27 @@
 import { assert } from 'chai';
 import { before } from 'mocha';
 import Web3 from 'web3';
-import { TransactionReceipt } from 'web3-eth';
-import { AbiItem } from 'web3-utils';
 import dotenv from 'dotenv';
 import ganache from 'ganache-core';
 
-import BlockNumber from './abis/BlockNumber.json';
-
-import { createStore } from './store';
+import { createStore } from '../store';
 import {
     Network,
     Block,
     BlockHeader,
     BlockTransaction,
     BlockTransactionObject,
-    Contract,
-    eventId,
-    CALL_BLOCK_SYNC,
-    CALL_TRANSACTION_SYNC,
     NetworkActions,
     BlockActions,
-    TransactionActions,
-    ContractActions,
     Web3ReduxActions,
     NetworkSelector,
     BlockSelector,
-    ContractSelector,
-} from './index';
-import { sleepForPort } from './utils';
-
-function sleep(ms: number) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
-}
+} from '../index';
+import { sleep, sleepForPort } from '../utils';
 
 const networkId = '1337';
 
-describe('sagas', () => {
+describe('block.sagas', () => {
     let web3Default: Web3;
     let web3: Web3; //Web3 loaded from store
     let accounts: string[];
@@ -221,185 +204,5 @@ describe('sagas', () => {
         subscription2.unsubscribe();
         state = store.getState();
         assert.deepEqual(state.web3Redux['Block'].itemsById, { ...expectedBlocks1, ...expectedBlocks2 });
-    });
-
-    it('store.dispatch(ContractSagas.call())', async () => {
-        //Block subsciption used for updates
-        store.dispatch(BlockActions.subscribe({ networkId }));
-        const tx = new web3.eth.Contract(BlockNumber.abi as AbiItem[]).deploy({
-            data: BlockNumber.bytecode,
-        });
-        const gas = await tx.estimateGas();
-        const contract = await tx.send({ from: accounts[0], gas, gasPrice: '10000' });
-
-        store.dispatch(
-            ContractActions.create({ networkId, address: contract.options.address, abi: BlockNumber.abi as AbiItem[] }),
-        );
-        store.dispatch(
-            ContractActions.call({
-                networkId,
-                address: contract.options.address,
-                method: 'blockNumber',
-                sync: false,
-            }),
-        );
-        await sleep(2000);
-
-        //@ts-ignore
-        const contractSel: Contract = ContractSelector.select(
-            store.getState(),
-            //@ts-ignore
-            `${networkId}-${contract.options.address}`,
-        );
-        const blockNumberKey = `().call(latest,{"from":"${accounts[0]}"})`;
-        const blockNumber1 = contractSel.methods!.blockNumber[blockNumberKey].value;
-        await sleep(2000);
-        const blockNumber2 = contractSel.methods!.blockNumber[blockNumberKey].value;
-        assert.equal(blockNumber1, blockNumber2);
-    });
-
-    it('store.dispatch(ContractSagas.call({sync:CALL_BLOCK_SYNC}))', async () => {
-        //Block subsciption used for updates
-        store.dispatch(BlockActions.subscribe({ networkId }));
-        const tx = new web3.eth.Contract(BlockNumber.abi as AbiItem[]).deploy({
-            data: BlockNumber.bytecode,
-        });
-        const gas = await tx.estimateGas();
-        const contract = await tx.send({ from: accounts[0], gas, gasPrice: '10000' });
-
-        store.dispatch(
-            ContractActions.create({ networkId, address: contract.options.address, abi: BlockNumber.abi as AbiItem[] }),
-        );
-        store.dispatch(
-            ContractActions.call({
-                networkId,
-                address: contract.options.address,
-                method: 'blockNumber',
-                sync: CALL_BLOCK_SYNC,
-            }),
-        );
-        await sleep(2000);
-
-        //@ts-ignore
-        const contractSel: Contract = ContractSelector.select(
-            store.getState(),
-            //@ts-ignore
-            `${networkId}-${contract.options.address}`,
-        );
-        const blockNumberKey = `().call(latest,{"from":"${accounts[0]}"})`;
-        const blockNumber1 = contractSel.methods!.blockNumber[blockNumberKey].value;
-        await sleep(2000);
-        const blockNumber2 = contractSel.methods!.blockNumber[blockNumberKey].value;
-        assert.notEqual(blockNumber1, blockNumber2);
-    });
-
-    it('store.dispatch(ContractSagas.call({sync:CALL_TRANSACTION_SYNC}))', async () => {
-        const tx1 = new web3.eth.Contract(BlockNumber.abi as AbiItem[]).deploy({
-            data: BlockNumber.bytecode,
-        });
-        const gas1 = await tx1.estimateGas();
-        const contract = await tx1.send({ from: accounts[0], gas: gas1, gasPrice: '10000' });
-        const tx2 = await contract.methods.setValue(42);
-        const gas2 = await tx2.estimateGas();
-        await tx2.send({ from: accounts[0], gas: gas2, gasPrice: '10000' });
-
-        store.dispatch(
-            ContractActions.create({ networkId, address: contract.options.address, abi: BlockNumber.abi as AbiItem[] }),
-        );
-        store.dispatch(
-            ContractActions.call({
-                networkId,
-                address: contract.options.address,
-                method: 'getValue',
-                sync: CALL_TRANSACTION_SYNC,
-            }),
-        );
-        await sleep(2000);
-
-        //@ts-ignore
-        const contractSel: Contract = ContractSelector.select(
-            store.getState(),
-            //@ts-ignore
-            `${networkId}-${contract.options.address}`,
-        );
-        const valueKey = `().call(latest,{"from":"${accounts[0]}"})`;
-        const value1 = contractSel.methods!.getValue[valueKey].value;
-        assert.equal(value1, 42);
-
-        const tx3 = await contract.methods.setValue(666);
-        const gas3 = await tx3.estimateGas();
-        const receipt: TransactionReceipt = await tx3.send({ from: accounts[0], gas: gas3, gasPrice: '10000' });
-        store.dispatch(
-            TransactionActions.fetch({
-                networkId,
-                hash: receipt.transactionHash,
-            }),
-        );
-
-        //Updated from transaction sync
-        await sleep(2000);
-        const value2 = contractSel.methods!.getValue[valueKey].value;
-        assert.equal(value2, 666);
-    });
-
-    it('store.dispatch(ContractSagas.send())', async () => {
-        const tx1 = new web3.eth.Contract(BlockNumber.abi as AbiItem[]).deploy({
-            data: BlockNumber.bytecode,
-        });
-        const gas1 = await tx1.estimateGas();
-        const contract = await tx1.send({ from: accounts[0], gas: gas1, gasPrice: '10000' });
-        store.dispatch(
-            ContractActions.create({ networkId, address: contract.options.address, abi: BlockNumber.abi as AbiItem[] }),
-        );
-
-        store.dispatch(
-            ContractActions.send({
-                networkId,
-                address: contract.options.address,
-                method: 'setValue',
-                args: [42],
-            }),
-        );
-
-        await sleep(2000);
-
-        const value = await contract.methods.getValue().call();
-        assert.equal(value, 42, 'setValue() did not work!');
-    });
-
-    it('store.dispatch(ContractSagas.eventSubscribe())', async () => {
-        const tx1 = new web3.eth.Contract(BlockNumber.abi as AbiItem[]).deploy({
-            data: BlockNumber.bytecode,
-        });
-        const gas1 = await tx1.estimateGas();
-        const contract = await tx1.send({ from: accounts[0], gas: gas1, gasPrice: '10000' });
-        store.dispatch(
-            ContractActions.create({ networkId, address: contract.options.address, abi: BlockNumber.abi as AbiItem[] }),
-        );
-
-        const expectedEvents: any = {};
-        contract.events['NewValue']().on('data', (event: any) => {
-            expectedEvents[eventId(event)] = event;
-        });
-        store.dispatch(
-            ContractActions.eventSubscribe({
-                networkId,
-                address: contract.options.address,
-                eventName: 'NewValue',
-            }),
-        );
-
-        const tx2 = await contract.methods.setValue(42);
-        const gas2 = await tx2.estimateGas();
-        await tx2.send({ from: accounts[0], gas: gas2, gasPrice: '10000' });
-
-        //@ts-ignore
-        const contractSel: Contract = ContractSelector.select(
-            store.getState(),
-            //@ts-ignore
-            `${networkId}-${contract.options.address}`,
-        );
-
-        assert.deepEqual(contractSel.events!.NewValue, expectedEvents);
     });
 });
